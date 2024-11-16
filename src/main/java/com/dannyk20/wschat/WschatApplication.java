ea97ed43dc6267c05031fb51f00a6cc957cea96b
dannyk20 <dannyk20@naver.com>
2024-11-16 14:50:03 +0900

    initial commit: Set up project structure

---
+++ src/main/java/com/dannyk20/wschat/WschatApplication.java
@@ -0,0 +1,11 @@
+package com.dannyk20.wschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WschatApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WschatApplication.class, args);
+	}
+}
