c121ddbdbd56f5d4e6c1317e84e8c32f1ccdfbc8
dannyk20 <dannyk20@naver.com>
2024-11-16 15:19:13 +0900

    [feat]:WebSocket service ChatRoomManager

---
+++ src/main/java/com/dannyk20/wschat/service/ChatRoomManager.java
@@ -0,0 +1,31 @@
+package com.dannyk20.wschat.service;
+
+import com.dannyk20.wschat.model.ChatRoom;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class ChatRoomManager {
+    private final Map<String, ChatRoom> chatRooms = new HashMap<>();
+
+    public ChatRoom createRoom(String roomId) {
+        ChatRoom room = new ChatRoom(roomId);
+        chatRooms.put(roomId, room);
+        return room;
+    }
+
+    public ChatRoom getRoom(String roomId) {
+        return chatRooms.get(roomId);
+    }
+
+    public void deleteRoom(String roomId) {
+        chatRooms.remove(roomId);
+    }
+
+    public Collection<ChatRoom> getChatRooms() {
+        return chatRooms.values();
+    }
+}
